24411339f13bf1f1bb933b9f42eec8964ff4a896
JavierB <javier.bastande@nuvolar.eu>
2025-09-15 21:47:13 +0200

    update file

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:2.3.4")
     implementation("io.ktor:ktor-server-netty-jvm:2.3.4")
     implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
 }
 
 tasks.register<Jar>("fatJar") {
